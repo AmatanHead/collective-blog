a8ca9b8ec55062049b0de837724248ead3edae72
Vladimir Goncharov <amatanhead@yandex-team.ru>
2016-09-22 15:12:38 +0300

    Register tag model in the admin interface

--- collective_blog/admin.py
+++ collective_blog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 from mptt.admin import MPTTModelAdmin
+from taggit.models import Tag as TaggitTag
 
-from collective_blog.models import Blog, Post, Membership, Comment
+from collective_blog.models import Blog, Post, Membership, Comment, Tag
 from s_markdown.admin import MarkdownAdmin
+
+
+admin.site.unregister(TaggitTag)
 
 
 @admin.register(Blog)
@@ -22,5 +26,10 @@ class CommentAdmin(MarkdownAdmin, MPTTModelAdmin):
 
 
 @admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
 class MembershipAdmin(admin.ModelAdmin):
     pass
